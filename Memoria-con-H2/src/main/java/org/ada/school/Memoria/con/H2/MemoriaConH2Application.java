832a9c30f4ba7bbe97b8ed018a94fc03a244d123
andreycaro <145929773+andreycaro@users.noreply.github.com>
2024-02-13 15:49:17 -0500

    Memoria-con-H2

---
+++ Memoria-con-H2/src/main/java/org/ada/school/Memoria/con/H2/MemoriaConH2Application.java
@@ -0,0 +1,13 @@
+package org.ada.school.Memoria.con.H2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaConH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriaConH2Application.class, args);
+	}
+
+}
---
+++ Memoria-con-H2/src/test/java/org/ada/school/Memoria/con/H2/MemoriaConH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.school.Memoria.con.H2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaConH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
